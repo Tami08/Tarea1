3d36b2c1d8323fbaac065f213fe95e6a5337b68c
Tami08 <145598398+Tami08@users.noreply.github.com>
2023-09-25 21:16:19 -0600

    tarea 1

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+'''
+Es una calculadora/ sumadora para dos numeros.
+Los numeros son decimales.
+'''
+
+print("hola mundo")
+nombre = input("Digite su nombre:  ")
+print("Bienvenido al curso: "+nombre)
+"""
+   PEP8 = Utilizar nombres descriptivos y en minusculas.
+   Para nombres compuestos, separar las palabras por guiones bajos.
+   Antes y despues del signo = debe haber uno (y solo un)
+"""
